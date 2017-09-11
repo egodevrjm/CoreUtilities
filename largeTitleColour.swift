034d4d404f9bbf9086ef0dded33bd223ab72d73e
Ryan Morrison <30383652+egodevrjm@users.noreply.github.com>
2017-09-11 23:40:13 +0100

    Create largeTitleColour.swift

---
+++ largeTitleColour.swift
@@ -0,0 +1,7 @@
+//in AppDelegate.swift inside didFinishLaunchingWithOptions put the follow
+
+UINavigationBar.appearance().largeTitleTextAttributes = [
+            NSAttributedStringKey.foregroundColor: UIColor.white
+            ]
+            
+// Makes the title colour of large Titles in iOS11 white
